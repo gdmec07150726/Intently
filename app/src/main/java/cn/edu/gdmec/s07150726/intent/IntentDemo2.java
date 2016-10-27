d291bc4dd916786604d80c00978713c7b528fae7
gdmec07150726 <2293210363@qq.com>
2016-10-27 20:34:34 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/s07150726/intent/IntentDemo2.java
@@ -0,0 +1,26 @@
+package cn.edu.gdmec.s07150726.intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2016/10/27.
+ */
+public class IntentDemo2 extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_intent_demo2);
+   }
+    public void upper(View v){
+        Intent intent=getIntent();
+        Bundle bundle=intent.getExtras();
+        final String value =bundle.getString("value").toUpperCase();
+        Intent intentresult=new Intent();
+        intentresult.putExtra("result",value);
+        setResult(RESULT_OK,intentresult);
+        finish();
+    }
+}
---
+++ app/src/main/java/cn/edu/gdmec/s07150726/intent/IntentDemo3.java
@@ -0,0 +1,16 @@
+package cn.edu.gdmec.s07150726.intent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/10/27.
+ */
+public class IntentDemo3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_intent_demo3);
+    }
+}
