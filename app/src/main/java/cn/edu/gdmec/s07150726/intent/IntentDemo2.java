0a63c51c4f0d29a278e2c3b388dbcf1fd163ea33
gdmec07150726 <2293210363@qq.com>
2016-12-26 17:20:50 +0800

    aa

--- app/src/main/java/cn/edu/gdmec/s07150726/intent/IntentDemo2.java
+++ app/src/main/java/cn/edu/gdmec/s07150726/intent/IntentDemo2.java
@@ -5,22 +5,21 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-/**
- * Created by Administrator on 2016/10/27.
- */
-public class IntentDemo2 extends AppCompatActivity{
+public class IntentDemo2 extends AppCompatActivity {
+
     @Override
-    protected void onCreate(Bundle saveInstanceState){
-        super.onCreate(saveInstanceState);
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent_demo2);
-   }
+    }
     public void upper(View v){
-        Intent intent=getIntent();
-        Bundle bundle=intent.getExtras();
-        final String value =bundle.getString("value").toUpperCase();
-        Intent intentresult=new Intent();
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        final String value = bundle.getString("value").toUpperCase();
+        Intent intentresult = new Intent();
         intentresult.putExtra("result",value);
         setResult(RESULT_OK,intentresult);
         finish();
     }
+
 }
